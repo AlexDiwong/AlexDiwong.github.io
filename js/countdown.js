455c1726fc4ab4ff42d9623c928e619c8cd52c12
Alexander Söderberg <email@alexandersoderberg.com>
2021-02-07 17:05:59 +0100

    Convert to wedding page

---
+++ js/countdown.js
@@ -0,0 +1,16 @@
+$(function () {
+    /* =========================================
+        COUNTDOWN 4
+     ========================================= */
+  function get15dayFromNow() {
+    return new Date(new Date("2021-10-02T14:30:00").valueOf());
+  }
+
+  $('#clock-c').countdown(get15dayFromNow(), function(event) {
+    var $this = $(this).html(event.strftime(''
+      + '<span class="h1 fancy-font-title">%D</span><span class="fancy-font-title">Day%!d</span>'
+      + '<span class="h1 fancy-font-title">%H</span><span class="fancy-font-title">Hr</span>'
+      + '<span class="h1 fancy-font-title">%M</span><span class="fancy-font-title">Min</span>'
+      + '<span class="h1 fancy-font-title">%S</span><span class="fancy-font-title">Sec</span>'));
+  });
+})

bddc9cbfd8da83ea92d62698eea86c3ed889ff48
Alexander Söderberg <email@alexandersoderberg.com>
2021-02-14 18:49:31 +0100

    Add q&a

--- js/countdown.js
+++ js/countdown.js
@@ -8,7 +8,8 @@ $(function () {
 
   $('#clock-c').countdown(get15dayFromNow(), function(event) {
     var $this = $(this).html(event.strftime(''
-      + '<span class="h1 text-center fancy-font-title" style="color: #fefefa">%D</span><span class="h2 fancy-font-title" style="color: #fefefa"> Day%!d Left</span>'
+      + '<span class="h1 text-center fancy-font-title display-3 d-none d-sm-block" style="color: #fefefa">%D</span><span class="h1 fancy-font-title display-3 d-none d-sm-block" style="color: #fefefa"> Day%!d Left</span>'
+      + '<span class="h1 text-center fancy-font-title d-xs-block d-sm-none" style="color: #fefefa">%D</span><span class="h1 fancy-font-title d-xs-block d-sm-none" style="color: #fefefa"> Day%!d Left</span>'
       // + '<span class="h1 fancy-font-title">%H</span><span class="fancy-font-title">Hr</span>'
       // + '<span class="h1 fancy-font-title">%M</span><span class="fancy-font-title">Min</span>'
       // + '<span class="h1 fancy-font-title">%S</span><span class="fancy-font-title">Sec</span>'
